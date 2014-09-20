444978f77232e038a012fa94a7cc025b76b273a1
Piri-Piri <david.pirih@gmx.de>
2014-09-20 02:17:06 +0200

    challenge2

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextfield: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sendMail(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = messageTextfield.text
+        messageLabel.textColor = UIColor.redColor()
+        messageTextfield.text = ""
+        messageTextfield.resignFirstResponder()
 
-
+        nameLabel.hidden = false
+        nameLabel.text = nameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        nameTextField.text = ""
+        nameTextField.resignFirstResponder()
+        sendButton.setTitle("Email send!", forState: UIControlState.Normal)
+    }
 }
 
